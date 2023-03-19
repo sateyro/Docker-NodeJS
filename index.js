55ede12c9de67ef4c6bc0f9242a5ecec80b32d8d
sateyro <121221414+sateyro@users.noreply.github.com>
2023-03-19 16:27:56 -0400

    Add NodeJs example project

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
